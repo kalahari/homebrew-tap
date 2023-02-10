d48d7f2b1df639d9f44ad8195e6f606cb11cc753
Blake Mitchell <blake.mitchell@wildflowerhealth.com>
2023-02-09 18:15:35 -0800

    dok v0.0.4

--- Formula/dok.rb
+++ Formula/dok.rb
@@ -1,8 +1,8 @@
 class Dok < Formula
     desc "CLI to manipulate the macOS Dock"
     homepage "https://github.com/kalahari/dok"
-    url "https://github.com/kalahari/dok/archive/refs/tags/v0.0.3.tar.gz"
-    sha256 "ab3f0668324550358a6780bfa91ea459b20d8ee51aa6a351bb02d2021ae6b692"
+    url "https://github.com/kalahari/dok/archive/refs/tags/v0.0.4.tar.gz"
+    sha256 "51ff7bed825e472867565522e70f6462d68f4ba57f6dcdc7847371efbe38bf19"
     license "MIT"
     head "https://github.com/kalahari/dok.git", branch: "main"
   
