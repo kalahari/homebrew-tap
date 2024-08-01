138041ad8dfa9a8271c88c19310259a093e16d69
Blake Mitchell <kalahari@users.noreply.github.com>
2024-08-01 08:57:21 -0700

    use the sum of the download not the redirect

--- Formula/dok.rb
+++ Formula/dok.rb
@@ -2,7 +2,7 @@ class Dok < Formula
     desc "CLI to manipulate the macOS Dock"
     homepage "https://github.com/kalahari/dok"
     url "https://github.com/kalahari/dok/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    sha256 "f2ee0ab5d90d25c8b381246c97b8372ea9ec31a7ab8c48c7c9e98f9ac47c1844"
     license "MIT"
     head "https://github.com/kalahari/dok.git", branch: "main"
   
